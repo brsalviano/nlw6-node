3fd5bf3055103f0f479c94e8ee38d3477276e9ce
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-07-26 19:58:11 -0300

    Configuração do banco com typeorm e criação de entidade

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,9 @@
 import express from 'express';
+import 'reflect-metadata';
+
+import './database';
 
 const app = express();
-
-app.get('/test', (req, res) => {
-  return res.send('Olá NLW 6');
-});
-
-app.post('/test-post', (req, res) => {
-  return res.send('NWL POST');
-});
 
 app.listen(3000, () => {
   console.log('Server is running NLW');
